84b256bcb03ac203caf4e4cf5292a9332fca24a4
Marina Ruiz Sánchez-Oro <marina_dorada@hotmail.es>
2019-02-21 22:56:11 +0000

    Make a basic component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p v-bind:style="changeColor">Hello {{ userName }}!</p>
+    <input id="jack" value="Jack" v-model="userName">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      userName: ''
+    }
+  },
+  computed: {
+    isMarina() {
+      if (this.userName == 'Marina') {
+        return true
+      }
+      return false
+    },
+    changeColor() {
+      if (this.isMarina) {
+        return {
+          color: 'red'
+        };
+      }
+      else {
+        return {
+          color: 'black'
+        };
+      }
+    }
+  }
+}
+</script>
